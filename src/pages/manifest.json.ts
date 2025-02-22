e029c44f2243b823926654c442b0bc8c803c2c19
Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
2025-02-22 01:21:36 -0600

    Squashed commit of the following:
    commit 4f380f1d09283b6c6d5f67a298a88f6f79cb49b5
    Author: Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
    Date:   Sat Feb 22 01:01:45 2025 -0600
    
        Fixed Case Issue
    
        - File names of the case change were being ignored. Had to use a temporary name change to force Git to recognize new case change. (from header.astro to Header.astro)
    
    commit aa8a4cb1da07d15817904c6eaa916d3bd9f55d55
    Author: Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
    Date:   Sat Feb 22 00:59:57 2025 -0600
    
        Temporary name change
    
    commit 385a91060bbefa83594919765faf92b18e140dc7
    Author: Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
    Date:   Sat Feb 22 00:46:26 2025 -0600
    
        Updated Import Paths
    
    commit 1fd1816d41f7ce0ba9becdf7911486401226883f
    Author: Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
    Date:   Sat Feb 22 00:36:00 2025 -0600
    
        Update tsconfig.json
    
    commit 60dcf32b866313a8eb81347371ef241f4b469be1
    Author: Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
    Date:   Sat Feb 22 00:30:32 2025 -0600
    
        Updated tsconifg
    
    commit d267aadc702b50d3b06f892562a0c8273b7340f0
    Author: Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
    Date:   Sat Feb 22 00:28:01 2025 -0600
    
        Updated import paths
    
    commit 5303a985493a0cd1b8356edcdb6b1b9cdca06ec7
    Author: Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
    Date:   Sat Feb 22 00:15:24 2025 -0600
    
        Fixed tsconfig
    
    commit 6034544ed28628a65ee9c014eab117981e9579a6
    Author: Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
    Date:   Fri Feb 21 23:50:09 2025 -0600
    
        Fixed tsconfig
    
    commit 5f7f34789378627fbc9c7858a7508ad32af27656
    Author: Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
    Date:   Fri Feb 21 23:17:40 2025 -0600
    
        Fixed tsconfig
    
    commit b8707e5a0040738f34b705a5cedf6fb003b597e6
    Author: Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
    Date:   Fri Feb 21 23:03:55 2025 -0600
    
        Updated Deps
    
    commit b7bd5344b43511069ae40e5038ca4368c041fced
    Author: Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
    Date:   Fri Feb 21 22:48:45 2025 -0600
    
        Updated Import Paths
    
    commit 39bad516d790611f7955f3c847940f6bdf6a2d39
    Author: Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
    Date:   Fri Feb 21 21:41:27 2025 -0600
    
        Resolved Paths and Astro Extensions
    
        - Fixed import paths not resolving correctly for Vercel build.
    
    commit b32c6d9a934bb2048444a5916a1cfbfa0f3bcd13
    Author: Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
    Date:   Fri Feb 21 21:26:06 2025 -0600
    
        Resolve Path
    
    commit 3ed0d2999c064b79507bb036ce3c82b5dd30ac12
    Author: Christopher Garza <43961806+cobaltwebtech@users.noreply.github.com>
    Date:   Fri Feb 21 21:18:56 2025 -0600
    
        Component Reorganization and Updates
    
        - Fixed favicons and added web manifest with maskable icons.
        - Reorganized and renamed some Astro components to match structure better.
        - Moved html head and meta tags into a separate BaseLayout.astro component.
        - Cleaned up some TS errors with ESLint
        - Updated Astro to 5.3.0 and other deps updated.

---
+++ src/pages/manifest.json.ts
@@ -0,0 +1,47 @@
+import type { APIRoute, ImageMetadata } from "astro";
+import icon from "@/assets/images/favicon/icon.png";
+import maskableIcon from "@/assets/images/favicon/icon-maskable.png";
+
+interface Favicon {
+  purpose: "any" | "maskable" | "monochrome";
+  src: ImageMetadata;
+  sizes: number[];
+}
+
+const sizes = [192, 512];
+const favicons: Favicon[] = [
+  {
+    purpose: "any",
+    src: icon,
+    sizes,
+  },
+  {
+    purpose: "maskable",
+    src: maskableIcon,
+    sizes,
+  },
+];
+
+export const GET: APIRoute = async () => {
+  const icons = favicons.flatMap((favicon) =>
+    sizes.map((size) => ({
+      src: `/icons/${favicon.purpose}-${size}.png`,
+      sizes: `${size}x${size}`,
+      type: "image/png",
+      purpose: favicon.purpose,
+    })),
+  );
+
+  const manifest = {
+    short_name: "Corner Lot",
+    name: "Dublin Corner Lot",
+    icons,
+    display: "minimal-ui",
+    id: "/",
+    start_url: "/",
+    theme_color: "#bae3b5",
+    background_color: "#214a1c",
+  };
+
+  return new Response(JSON.stringify(manifest));
+};
